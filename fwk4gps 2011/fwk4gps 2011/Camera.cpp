e3689ff150978f8318dbb8be504a0981861cada3
gbatumbya <Grace.Batumbya@senecac.on.ca>
2011-11-18 05:39:48 -0500

    Started working on dialogbox. Enabled basic jumping

--- fwk4gps 2011/fwk4gps 2011/Camera.cpp
+++ fwk4gps 2011/fwk4gps 2011/Camera.cpp
@@ -7,6 +7,8 @@
  * copyright (c) 2011 Chris Szalwinski 
  * distributed under TPL - see ../Licenses.txt
  */
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 #include "iContext.h"        // for the Context Interface
 #include "iCoordinator.h"    // for the Coordinator Interface
@@ -16,7 +18,6 @@
 #include "Camera.h"          // for Camera class definition
 #include "MathDefinitions.h" // for math functions in coordinator coordinates
 #include "ModelSettings.h"   // for FLOOR, FORWARD_SPEED, ROT_SPEED, MAX_DESC
-
 //-------------------------------- Camera -------------------------------------
 //
 // The Camera class defines the structure of one viewpoint on the scene
@@ -29,14 +30,14 @@ iCoordinator* Camera::coordinator = NULL;
 //
 iCamera* CreateCamera(iContext* c, Maze* m) {
 
-	return new Camera(c, m);
+   return new Camera(c, m);
 }
 
 // constructor adds the Camera Instance to the coordinator
 //
 Camera::Camera(iContext* c, Maze* m) : context(c) {
 
-	maze = m;
+   maze = m;
     if (!coordinator)
         error(L"Camera::00 Couldn\'t access the Coordinator");
     else if(!coordinator->add(this))
@@ -51,109 +52,103 @@ Camera::Camera(iContext* c, Maze* m) : context(c) {
 // to the user's interventions
 //
 void Camera::update(int delta) {
+   static bool jumping = false;
+   static float last = 0;
 
-    int dx = 0, // pitch up/down
+   int dx = 0, // pitch up/down
         dy = 0, // yaw left/right
         dz = 0; // advance/retreat
     int rx = 0,
         ry = 0,
         rz = 0;
-	float bounds = 0.2,
-		  boundingX = 0,
-		  boundingY = 0;
-    bool adjustCamera = !context->pressed(ADJUST_PROJECTOR);
+   float bounds = 0.2,
+        boundingX = 0,
+        boundingY = 0;
 
-    // controller input
-    int jx = context->get(GF_CT_POSX);
-    int jy = context->get(GF_CT_POSY);
-    int jr = context->get(GF_CT_ROTZ);
-    if (jy)
-        rx = -(int)(jy * CTR_SPEED);
-    if (jr)
-        ry = (int)(jr * CTR_SPEED);
-    if (jx)
-        rz = (int)(jx * CTR_SPEED);
-
-	// mouse input
+   // mouse input
     int mx = context->get(GF_MS_DSPX);
     int my = context->get(GF_MS_DSPY);
     int mz = context->get(GF_MS_ROTZ);
-	if (mx)
-		ry += mx * MOUSE_SPEED;
-	if (my)
-		rx += my * MOUSE_SPEED;
-	if (mz)
-		dy += mz * MOUSE_SPEED;
-		
-	// keyboard input
+   if (mx)
+      ry += mx * MOUSE_SPEED;
+   if (my)
+      rx += my * MOUSE_SPEED;
+   if (mz)
+      rz += mz * MOUSE_SPEED;
+      
+   // keyboard input
     if (context->pressed(CAM_STRAFE_LEFT))
         dx -= delta;
     if (context->pressed(CAM_STRAFE_RIGHT))
         dx += delta;
-    if (context->pressed(CAM_STRAFE_LEFT_ALT))
-        dx -= delta;
-    if (context->pressed(CAM_STRAFE_RIGHT_ALT))
-        dx += delta;
-    if (context->pressed(CAM_FLY_DOWN))
+/*    if (context->pressed(CAM_FLY_DOWN))
         dy -= delta;
     if (context->pressed(CAM_FLY_UP))
-        dy += delta;
+        dy += delta*/;
     if (context->pressed(CAM_ADVANCE))
         dz += delta;
     if (context->pressed(CAM_RETREAT))
         dz -= delta;
-    if (context->pressed(CAM_PITCH_UP))
-        rx -= delta;
-    if (context->pressed(CAM_PITCH_DOWN))
-        rx += delta;
-    //if (context->pressed(CAM_YAW_LEFT))
-    //    ry -= delta;
-    //if (context->pressed(CAM_YAW_RIGHT))
-    //    ry += delta;
-    //if (context->pressed(CAM_ROLL_LEFT))
-    //    rz -= delta;
-    //if (context->pressed(CAM_ROLL_RIGHT))
-    //    rz += delta;
+    if (context->pressed(CAM_JUMP) && jumping == false)
+    {
+      last = 0;
+      jumping = true;
+    }
+
+    if (jumping)
+    {
+       last += (delta/1000.0f);
+       dy += SCALE * sinf(3.1415927f * last / 2);
+    }
 
     // adjust camera orientation
-	if (adjustCamera && (rx || ry || rz)) {
+    if (rx || ry || rz) {
         // yaw left/right
-		if (ry)
+      if (ry)
             rotatey(-ry * ANG_CAM_SPEED);
-		// pitch up/down
+      // pitch up/down
         if (rx) 
-            rotate(orientation('x'), rx * ANG_CAM_SPEED);
-		// roll left/right
-        if (rz) 
-            rotate(orientation('z'), rz * ANG_CAM_SPEED);
+           rotate(orientation('x'), rx * ANG_CAM_SPEED);
+      //// roll left/right
+      //  if (rz) 
+      //      rotate(orientation('z'), rz * ANG_CAM_SPEED);
     }
-	// adjust camera position
-    if (adjustCamera && (dx || dy || dz)) {
+   // adjust camera position
+    if (dx || dy || dz) {
 
-        Vector displacement = 
-         (float) dx * CAM_SPEED * orientation('x') +
-         Vector(0,0,0) + 
-         (float) dz * CAM_SPEED * orientation('z');
+      Vector displacement = 
+      (float) dx * CAM_SPEED * orientation('x') +
+      (float) dy * CAM_SPEED * orientation('y') + 
+      (float) dz * CAM_SPEED * orientation('z');
 
-		Vector pos = position();
+      translate(0, displacement.y, 0);
 
-		if (displacement.z > 0) {
-		
-			boundingY = bounds;
-		} else {
-		
-		    boundingY = -bounds;
-		}
-		if (displacement.x > 0) {
-		
-			boundingX = bounds;
-		} else {
-		
-		    boundingX = -bounds;
-		}
+      Vector pos = position();
 
-		if (!maze->checkCollision((pos.x + displacement.x) / SCALE + boundingX, pos.z / SCALE)) translate(displacement.x, 0, 0);
-        if (!maze->checkCollision(pos.x / SCALE, (pos.z + displacement.z) / SCALE + boundingY)) translate(0, 0, displacement.z);
+      if (pos.y < 4.0f)
+      {
+         translate(0, 4.0f - pos.y, 0);
+         jumping = false;
+      }
+
+      if (displacement.z > 0) {
+      
+         boundingY = bounds;
+      } else {
+      
+          boundingY = -bounds;
+      }
+      if (displacement.x > 0) {
+      
+         boundingX = bounds;
+      } else {
+      
+          boundingX = -bounds;
+      }
+
+      if (!maze->checkCollision((pos.x + displacement.x) / SCALE + boundingX, pos.z / SCALE) && 
+          !maze->checkCollision(pos.x / SCALE, (pos.z + displacement.z) / SCALE + boundingY)) 
+          translate(displacement.x, 0, displacement.z);
     }
 
     // store the current viewpoint, heading and up direction
